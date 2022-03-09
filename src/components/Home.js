63506c4994add9dcf542fde82b0742870fb09578
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-03-10 01:24:55 +0600

    redux-linkup

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Home() {
+
+  return (
+
+    
+    <div>
+
+<h3>React Componet From Home </h3>
+<div className="cart-wrapper">
+    <div className="img-wrapper items">
+        
+        <img src="https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1639632985.13488695.jpg" alt="" srcset="" />
+        
+        </div>
+</div>
+
+    </div>
+  )
+
+}
+
+export default Home
